b3674ec72aacd61ca214c6bfbb5fa1b452bc7343
Musa Omur <dunedenmusa@gmail.com>
2024-04-09 13:59:25 -0400

    create and update classes are created

--- java/src/main/java/com/techelevator/dao/JdbcPetDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcPetDao.java
@@ -1,6 +1,9 @@
 package com.techelevator.dao;
 
+import com.techelevator.exception.DaoException;
 import com.techelevator.model.Pet;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
@@ -11,8 +14,9 @@ import java.util.List;
 
 @Component
 public class JdbcPetDao implements PetDao{
-    private final String PET_SELECT ="SELECT pet_id, species_id, name, age, color, has_special_needs, " +
-            "breed, size, gender, description, is_fixed, is_adopted FROM pets;";
+    private final String PET_SELECT ="SELECT pet_id, species_id, name, age, color, " +
+            "has_special_needs, breed, size, gender, description, is_fixed, " +
+            "is_adopted FROM pets";
     private JdbcTemplate jdbcTemplate;
 
     public JdbcPetDao(DataSource dataSource) {
@@ -28,6 +32,69 @@ public class JdbcPetDao implements PetDao{
         }
         return pets;
     }
+
+    @Override
+    public Pet getPetById(int petId) {
+        Pet aPet = null;
+        String sql = PET_SELECT + " WHERE pet_id = ?";
+        try {
+            SqlRowSet results = jdbcTemplate.queryForRowSet(sql, petId);
+            if (results.next()) {
+                aPet = mapRowToPet(results);
+            }
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Unable to connect to server or database", e);
+        }
+        return aPet;
+    }
+    @Override
+    public Pet createPet(Pet aPet) {
+        String sql = "INSERT INTO pets (species_id, name, age, color, has_special_needs, breed, " +
+                "size, gender, description, is_fixed, is_adopted) " +
+                "VALUES (?,?,?,?,?,?,?,?,?,?,?) RETURNING pet_id;";
+        try {
+            int id = jdbcTemplate.queryForObject(
+                    sql, int.class, aPet.getSpeciesId(), aPet.getName(), aPet.getAge(),
+                    aPet.getColor(), aPet.getHasSpecialNeed(), aPet.getBreed(),
+                    aPet.getSize(), aPet.getGender(), aPet.getDescription(), aPet.getIsFixed(),
+                    aPet.getIsAdopted()
+            );
+
+            aPet = getPetById(id);
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Unable to connect to server or database", e);
+        } catch (DataIntegrityViolationException e) {
+            throw new DaoException("Data integrity violation", e);
+        }
+        return aPet;
+    }
+
+    @Override
+    public Pet updatePet(Pet aPet) {
+        Pet updatedPet = null;
+        String sql = "UPDATE pets SET species_id = ?, name = ?, age = ?, " +
+                "has_special_needs = ?, color = ?, breed = ?, size = ?, gender = ?, " +
+                "description = ?, is_fixed = ?, is_adopted = ?  WHERE pet_id = ?;";
+        try {
+            int numberOfRows = jdbcTemplate.update(
+                    sql, aPet.getSpeciesId(), aPet.getName(), aPet.getAge(), aPet.getHasSpecialNeed(),
+                    aPet.getColor(), aPet.getBreed(), aPet.getSize(), aPet.getGender(),
+                    aPet.getDescription(), aPet.getIsFixed(), aPet.getIsAdopted(), aPet.getPetId()
+            );
+
+            if (numberOfRows == 0) {
+                throw new DaoException("Zero rows affected, expected at least one");
+            } else {
+                updatedPet = getPetById(aPet.getPetId());
+            }
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Unable to connect to server or database", e);
+        } catch (DataIntegrityViolationException e) {
+            throw new DaoException("Data integrity violation", e);
+        }
+        return updatedPet;
+    }
+
     private Pet mapRowToPet(SqlRowSet sqlRowSet) {
         Pet pet = new Pet();
         pet.setPetId(sqlRowSet.getInt("pet_id"));
--- java/src/main/java/com/techelevator/dao/JdbcUserDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcUserDao.java
@@ -88,9 +88,12 @@ public class JdbcUserDao implements UserDao {
         return newUser;
     }
 
+
+
+
     private User mapRowToUser(SqlRowSet rs) {
         User user = new User();
-        user.setId(rs.getInt("user_id"));
+        user.setId(rs.getInt("user_iDd"));
         user.setUsername(rs.getString("username"));
         user.setPassword(rs.getString("password_hash"));
         user.setAuthorities(Objects.requireNonNull(rs.getString("role")));
