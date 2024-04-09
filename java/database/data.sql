bd6737caf20357860127d608f06c9362275070bd
Jennifer Smale <JenniferSmale7@gmail.com>
2024-04-08 22:26:11 -0400

    complete DB

--- java/database/data.sql
+++ java/database/data.sql
@@ -3,6 +3,11 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+INSERT INTO people (user_id, first_name, last_name, email, is_available_weekdays, is_available_weekends, volunteering_interest)
+	VALUES (1, 'John', 'Smith', 'JohnSmith@volunteer.com', false, true, null);
+INSERT INTO people (user_id, first_name, last_name, email, is_available_weekdays, is_available_weekends, volunteering_interest)
+	VALUES (2, 'Jane', 'Doe', 'JaneTheAdmin@boss.com', true, true, 'I love animals and cannot wait to make a difference in the world!');
+
 INSERT INTO species (name) VALUES ('cat');
 INSERT INTO species (name) VALUES ('dog');
 INSERT INTO species (name) VALUES ('bunny');
@@ -25,5 +30,31 @@ INSERT INTO pets (species_id, name, age, has_special_needs, color, breed, size, 
 	VALUES (3, 'Carrots', 5, false, 'white, black', 'lionhead', 'XS', 'Female', false, false, 'Believe it or not, Carrots LOVES... carrots!');
 INSERT INTO pets (species_id, name, age, has_special_needs, color, breed, size, gender, is_fixed, is_adopted, description) 
 	VALUES (3, 'Sir Hops-A-Lot', 1, true, 'white', 'mini lop', 'L', 'Male', true, false, 'Gets around great but is a blind boy.');
+
+
+INSERT INTO photos (file_name, pet_id)
+	VALUES ('..\src\main\resources\cats\placeholder_cat.JPG', 1);
+INSERT INTO photos (file_name, pet_id)
+	VALUES ('..\src\main\resources\dogs\placeholder_dog.JPG', 1);
+INSERT INTO photos (file_name, pet_id)
+	VALUES ('..\src\main\resources\bunnies\placeholder_rabbit.JPG', 1);
+INSERT INTO photos (file_name, pet_id)
+	VALUES ('..\src\main\resources\cats\Cat 1.JPG', 1);
+INSERT INTO photos (file_name, pet_id)
+	VALUES ('..\src\main\resources\cats\Cat 2.JPG', 2);
+INSERT INTO photos (file_name, pet_id)
+	VALUES ('..\src\main\resources\cats\Cat 3.JPG', 3);
+INSERT INTO photos (file_name, pet_id)
+	VALUES ('..\src\main\resources\dogs\Dog 1.JPG', 4);
+INSERT INTO photos (file_name, pet_id)
+	VALUES ('..\src\main\resources\dogs\Dog 2.JPG', 5);
+INSERT INTO photos (file_name, pet_id)
+	VALUES ('..\src\main\resources\dogs\Dog 3.JPG', 6);
+INSERT INTO photos (file_name, pet_id)
+	VALUES ('..\src\main\resources\bunnies\Rabbit 1.JPG', 7);
+INSERT INTO photos (file_name, pet_id)
+	VALUES ('..\src\main\resources\bunnies\Rabbit 2.JPG', 8);
+INSERT INTO photos (file_name, pet_id)
+	VALUES ('..\src\main\resources\bunnies\Rabbit 3.JPG', 9);
 
 COMMIT TRANSACTION;
