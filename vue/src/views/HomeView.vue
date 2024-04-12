d2442db54dd1678b9478e505a72436cca43694c9
Jennifer Smale <JenniferSmale7@gmail.com>
2024-04-12 14:54:37 -0400

    update JdbcPersonDao, App.vue, HomeView.vue

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,45 +1,65 @@
 <template>
-    <div class="About-us">
-        <!--this section explains the about us on the home page--> 
-        <h2 id="Who-title"> Who are we?</h2>
-        <p class="about-desc"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eu sapien non enim imperdiet vulputate. In rhoncus, purus ac sodales porta, erat felis posuere mi, at ullamcorper lorem libero id urna. Morbi hendrerit quam vitae neque luctus tincidunt. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam dignissim elit ac dolor luctus, sit amet fermentum sem varius. Integer luctus, est ut eleifend tempus, dui libero rhoncus odio, pharetra ultricies tellus velit sit amet magna. Vestibulum non nulla et purus blandit laoreet eget in diam. Praesent vitae quam non eros ullamcorper bibendum. Praesent ullamcorper, tellus sed cursus faucibus, est arcu auctor massa, dictum cursus quam sapien iaculis enim. </p>
+
+    <div class="about-us">
+        <h2 id="about-title">Welcome to Paw Prints Pet Rescue!</h2>
+        <p class="about-desc"> 
+            We believe every animal deserves a chance at happiness. Founded in 2024 in Cleveland, OH, we're dedicated to rescuing, rehabilitating, and rehoming pets in need. Our community of staff and volunteers works tirelessly to provide care, love, and a second chance for animals, connecting them with families ready to open their hearts and homes. Together, we can ensure every pet finds a path to love and safety.
+        </p>
+        <img src="/Paw Prints Pet Rescue Logo.png" class="company-logo" alt="PAW LOGO">
     </div>
-            <!-- This will toss a title and description on the home page letting the user see the pets that have already been adopted -->
-            <div class="adopted-pet">
+
+    <div class="adopted-pet">
         <h2 id="pet-title">Success Stories</h2>
         <p class="adopted-desc">
             These stories inspire community involvement in adoption, underscore the tireless efforts of our staff and volunteers, and emphasize the importance of providing loving homes to needy animals. It's a testament to the power of companionship and the positive impact of giving pets a second chance at happiness.
-        </p> 
-        
+        </p>    
     </div>
     <div class="pet-box">
-    <pets-list :is-adopted="true"/>
-</div>
+        <pets-list :is-adopted="true"/>
+    </div>
 </template>
 
 <script>
 import PetsList from "@/components/PetsList.vue";
 
 export default {
-    components: {PetsList}
+    components: {
+        PetsList
+    }
 };
 </script>
 <style>
-.About-us{
+.about-us{
     border: solid #392F5a 3px;
     background-color: #9DD9D2;
     padding: 10px;
     margin-bottom: 5px;
+    display: grid;
+    grid-template-columns: 3fr 1fr;
+    grid-template-areas: 
+    "title pic"
+    "about pic";
+    justify-content: center;
+    align-items: center;
 }
-#Who-title{
+
+.company-logo{
+    grid-area: pic;
+    max-height: 30vh;
+    padding-left: 5px;
+}
+#about-title{
+    grid-area: title;
     display: flex;
-    justify-content: left;
 }
+.about-desc {
+    grid-area: about;
+}
+
 
 #pet-title{
     display: flex;
     justify-content: left;
-    
 }
 .adopted-pet{
     border: solid #392F5a 3px;
