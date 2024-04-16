5e65cacef8a485d64e862c4c23d15458d7492f0c
Jennifer Smale <JenniferSmale7@gmail.com>
2024-04-16 16:15:29 -0400

    update styling across front end

--- vue/src/components/PetsList.vue
+++ vue/src/components/PetsList.vue
@@ -4,51 +4,69 @@
     </div>
     <div v-else>
 
-        <div class="pet-filter">
+        <div id="filter-toggle">
+            <button id="toggle-button" @click="toggleFilters">Filter Pets</button>
+        </div>
 
-            <label for="speciesId">Species:</label>
-            <select id="speciesId" v-model="filters.speciesId">
-                <option value="">All Species</option>
-                <option value="1">Cats</option>
-                <option value="2">Dogs</option>
-                <option value="3">Bunnies</option>
-            </select>
+        <div v-if="showFilters" class="pet-filter">
+            <div class="species-filter">
+                <label for="speciesId">Species:</label>
+                <select id="speciesId" v-model="filters.speciesId">
+                    <option value="">All Species</option>
+                    <option value="1">Cats</option>
+                    <option value="2">Dogs</option>
+                    <option value="3">Bunnies</option>
+                </select>
+            </div>
+            
+            <div class="age-filter">
+                <label for="age">Age:</label>
+                <input id="age" type="number" v-model.number="filters.age" placeholder="Age" max="30" min="0">
+            </div>
 
-            <label for="age">Age:</label>
-            <input id="age" type="number" v-model.number="filters.age" placeholder="Age">
+            <div class="breed-filter">
+                <label for="breed">Breed:</label>
+                <input id="breed" v-model="filters.breed" placeholder="Breed">
+            </div>
 
-            <label for="breed">Breed:</label>
-            <input id="breed" v-model="filters.breed" placeholder="Breed">
+            <div class="color-filter">
+                <label for="color">Color:</label>
+                <input id="color" v-model="filters.color" placeholder="Color">
+            </div>
 
-            <label for="color">Color:</label>
-            <input id="color" v-model="filters.color" placeholder="Color">
+            <div class="size-filter">
+                <label for="size">Size:</label>
+                <select id="size" v-model="filters.size">
+                    <option value="">All Sizes</option>
+                    <option value="XS">XS</option>
+                    <option value="S">S</option>
+                    <option value="M">M</option>
+                    <option value="L">L</option>
+                    <option value="XL">XL</option>
+                </select>
+            </div>
 
-            <label for="size">Size:</label>
-            <select id="size" v-model="filters.size">
-                <option value="">All Sizes</option>
-                <option value="XS">XS</option>
-                <option value="S">S</option>
-                <option value="M">M</option>
-                <option value="L">L</option>
-                <option value="XL">XL</option>
-            </select>
+            <div class="gender-filter">
+                <label for="gender">Gender:</label>
+                <select id="gender" v-model="filters.gender">
+                    <option value="">All Genders</option>
+                    <option value="Female">Female</option>
+                    <option value="Male">Male</option>
+                </select>
+            </div>
 
-            <label for="gender">Gender:</label>
-            <select id="gender" v-model="filters.gender">
-                <option value="">All Genders</option>
-                <option value="Female">Female</option>
-                <option value="Male">Male</option>
-            </select>
+            <div class="fixed-filter">
+                <label for="isFixed">Fixed:</label>
+                <input id="isFixed" type="checkbox" v-model="filters.isFixed">
+            </div>
 
-            <label for="isFixed">Fixed:</label>
-            <input id="isFixed" type="checkbox" v-model="filters.isFixed">
-
-            <label for="hasSpecialNeed">Special Needs:</label>
-            <input id="hasSpecialNeed" type="checkbox" v-model="filters.hasSpecialNeed">
+            <div class="special-filter">
+                <label for="hasSpecialNeed">Special Needs:</label>
+                <input id="hasSpecialNeed" type="checkbox" v-model="filters.hasSpecialNeed">
+            </div>            
 
             <!-- <label for="isAdopted">Show Adopted:</label>
             <input id="isAdopted" type="checkbox" v-model="filters.isAdopted"> -->
-
         </div>
 
         <div class="pets-list">
@@ -76,6 +94,7 @@ export default {
         return {
             isLoading: true,
             pets: [],
+            showFilters: false,
             filters: {
                 speciesId: '',
                 breed: '',
@@ -89,6 +108,11 @@ export default {
             }
         }
     },
+    methods: {
+        toggleFilters() {
+            this.showFilters = !this.showFilters;
+        }
+    },
     created() {
         petService.getPets(this.isAdopted).then(response => {
             this.pets = response.data;
@@ -100,9 +124,9 @@ export default {
         filteredPets() {
             return this.pets.filter(pet => {
                 return (!parseInt(this.filters.speciesId) || pet.speciesId === parseInt(this.filters.speciesId))
-                    && (!this.filters.breed || pet.breed.toLowerCase() === (this.filters.breed.toLowerCase()))
+                    && (!this.filters.breed || pet.breed.toLowerCase().includes(this.filters.breed.toLowerCase()))
                     && (!this.filters.age || pet.age === this.filters.age)
-                    && (!this.filters.color || pet.color.toLowerCase() === (this.filters.color.toLowerCase()))
+                    && (!this.filters.color || pet.color.toLowerCase().includes(this.filters.color.toLowerCase()))
                     && (!this.filters.size || pet.size.toLowerCase() === (this.filters.size.toLowerCase()))
                     && (!this.filters.gender || pet.gender.toLowerCase() === this.filters.gender.toLowerCase())
                     && (!this.filters.isFixed || pet.isFixed === this.filters.isFixed)
@@ -115,6 +139,24 @@ export default {
 </script>
 
 <style scoped>
+#filter-toggle {
+    display: flex;
+    justify-content: end;
+    padding: 1vh;
+}
+#toggle-button {
+    background-color: #F4D06F;
+    border: solid #392F5A 2px;
+    color: #392F5A;
+    text-align: center;
+    text-decoration: none;
+    font-family: Dekko;
+    font-size: 115%;
+}
+#toggle-button:hover {
+    background-color: #9DD9D2;
+}
+
 .pet-filter {
     margin-top: 2vh;
     display: flex;
@@ -126,12 +168,13 @@ export default {
   align-content: center;
   grid-template-columns: 1fr 1fr 1fr;
   gap: 10px;
-  padding: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
 }
 
 .pets-list > div {
   text-align: center;
-  padding: 20px 0;
+  padding: 5px 0;
   font-size: 30px;
 }
 </style>
