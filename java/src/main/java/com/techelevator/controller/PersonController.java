a0f31abf8948c3da6f6e6c34caed935e23de6e0b
Mykyta <mp091689@gmail.com>
2024-04-16 07:42:38 -0400

    fix creation of person

--- java/src/main/java/com/techelevator/controller/PersonController.java
+++ java/src/main/java/com/techelevator/controller/PersonController.java
@@ -51,6 +51,8 @@ public class PersonController {
         }
 
         person.setToken(UUID.randomUUID().toString());
+        person.setIsApproved(null);
+
         return personDao.createPerson(person);
     }
 
